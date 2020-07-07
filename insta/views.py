7805ea9d641b429ed9a5d7fd3a664fb75d153816
safwanvk <safwanvalakundil@gmail.com>
2020-07-07 15:50:26 +0530

    Create template post_detail

--- insta/views.py
+++ insta/views.py
@@ -11,3 +11,5 @@ class PostListView(ListView):
     template_name = 'insta/home.html'
     queryset = Post.objects.all().filter(created_date__lte=timezone.now()).order_by('-created_date')
     context_object_name = 'posts'
+
+
