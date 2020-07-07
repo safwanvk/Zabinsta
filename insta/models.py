a8ffd44ff872ad0a9b52fbac44c82b941c76f58b
safwanvk <safwanvalakundil@gmail.com>
2020-07-07 15:35:54 +0530

    Modified view & model

--- insta/models.py
+++ insta/models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 from django.utils import timezone
 
@@ -12,8 +11,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(blank=True, null=True)
     caption = models.TextField()
+    likes = models.ManyToManyField(User, blank=True, related_name='post_likes')
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.CharField(max_length=50)
 
     def __str__(self):
         return self.caption
@@ -25,19 +24,3 @@ class Post(models.Model):
         except:
             url = ''
         return url
-
-    @property
-    def published(self):
-        created_dates = self.created_date.replace(tzinfo=None)
-        Now = datetime.now()
-        Now = Now.replace(microsecond=0)
-        time_difference = Now - created_dates
-        if time_difference.days == 0:
-            if int(time_difference.seconds / 3600) == 0:
-                self.published_date = str(int(time_difference.seconds / 60)) + " MINUTES AGO"
-            else:
-                self.published_date = str(int(time_difference.seconds / 3600)) + " HOURS AGO"
-        else:
-            self.published_date = str(int(time_difference.days)) + " DAYS AGO"
-        self.save()
-        return self.published_date
--- insta/views.py
+++ insta/views.py
@@ -8,32 +8,6 @@ from insta.models import Post
 
 
 class PostListView(ListView):
-    template_name = "insta/home.html"
+    template_name = 'insta/home.html'
     queryset = Post.objects.all().filter(created_date__lte=timezone.now()).order_by('-created_date')
     context_object_name = 'posts'
-
-    for post in queryset:
-        post.published
-
-    def posted_time_onIG(self):
-        posts = Post.objects.all()
-        for post in posts:
-            Id = post.id
-            print(post)
-            # print(post_id)
-            Now = datetime.now()
-            time_created = post[Id - 1].created_date
-            if (Now.day - post.created_date.day == 0):
-                if (Now.hour - post.created_date.hour == 0):
-                    posted_time = str(Now.minute - time_created.minute) + " MINUTES AGO"
-                    print("------------>", posted_time, time_created)
-                else:
-                    posted_time = str(Now.hour - time_created.hour) + " HOURS AGO"
-                    print("------------>", posted_time, time_created)
-
-            else:
-                posted_time = str(Now.day - time_created.day) + " DAYS AGO maybe wrong  " + str(
-                    Now.day - post.created_date.day) + "post id = " + str(post.id)
-                print("------------>", posted_time)
-            return posted_time
-            print("....................................................................")
