22451c6f08293a47bff8880f2054903a123b00f5
safwanvk <safwanvalakundil@gmail.com>
2020-07-07 21:01:12 +0530

    Update form UserCreationForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 
 class UserCreationForm(UserCreationForm):
-    model = User
-    fields = '__all__'
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username',
+                  'email',
+                  'password',
+                  'password2',
+                  ]
