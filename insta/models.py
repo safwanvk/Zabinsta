a886704a34df6a54583c8fc2f21d44c0f63d0c76
safwanvk <safwanvalakundil@gmail.com>
2020-07-08 20:57:26 +0530

    Set model Comment

--- insta/models.py
+++ insta/models.py
@@ -29,3 +29,16 @@ class Post(models.Model):
         except:
             url = ''
         return url
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.commenter)
