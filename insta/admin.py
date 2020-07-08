d5d4de725f667ec52262c2a9470ea5668a958e20
safwanvk <safwanvalakundil@gmail.com>
2020-07-08 21:14:04 +0530

    Set comment view

--- insta/admin.py
+++ insta/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from insta.models import Post
+from insta.models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
